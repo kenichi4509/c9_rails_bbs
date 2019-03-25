c923bfd11ee4436316b94d302c71c71ce722f7f3
kenichi4509 <kenichi.y4509@gmail.com>
2019-03-25 12:35:05 +0000

    ルートの内容変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "post_lists#index"
+  root "boards#index"
 end
