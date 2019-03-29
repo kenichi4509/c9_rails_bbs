e6b2e0fbe4f63b53df53ce6717c81a0a14b86290
kenichi4509 <kenichi.y4509@gmail.com>
2019-03-29 03:21:36 +0000

    boards#newアクションのテスト

--- spec/controllers/boards_controller_spec.rb
+++ spec/controllers/boards_controller_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe BoardsController, type: :controller do
 
+  describe 'Get #new' do
+    before { get :new } 
+      
+      it 'レスポンスコードが200であること' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'newテンプレートにレンダリングできること' do
+        expect(response).to render_template :new
+      end
+
+  end
 end
