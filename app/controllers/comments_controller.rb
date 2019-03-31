4405d624e86f80543bc6eb94f8fdcc46fc246b76
kenichi4509 <kenichi.y4509@gmail.com>
2019-03-31 23:30:54 +0000

    createアクションの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,13 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to comment.board, flash: { notice: 'コメントを投稿しました'  }
+    else
+      flash[:comment] = comment
+      flash[:error_messages] = comment.errors.full_messages
+      redirect_back fallback_location: comment.board
+    end
   end
 
   def edit
@@ -10,4 +18,12 @@ class CommentsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:board_id, :name, :comment)
+  end
+
+
 end
