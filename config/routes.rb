837bf5f18abf891e6be48667896545b88a023475
kenichi4509 <kenichi.y4509@gmail.com>
2019-04-06 07:59:07 +0000

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'mypage', to: 'users#new'
+  get 'mypage', to: 'users#me'
   post 'login', to:  'sessions#create'
   delete 'logout', to:  'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
