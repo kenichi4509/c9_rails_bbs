372add8bce5bfb758d7db54836343b1a2fe78ac4
kenichi4509 <kenichi.y4509@gmail.com>
2019-04-07 05:27:03 +0000

    ログイン時のエラー表示を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to mypage_path
     else
-      render 'home/index'
+      #render 'home/index'
+      redirect_to users_path, flash: { notice: "ログインに失敗しました"}
     end
   end
 
