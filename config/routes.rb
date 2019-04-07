899290325451111edef7e4918da12c51eaa2b39b
kenichi4509 <kenichi.y4509@gmail.com>
2019-04-07 05:31:22 +0000

    rootの変更、usersのアクションにindexを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   post 'login', to:  'sessions#create'
   delete 'logout', to:  'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  resources :users, only: %i[new create]
+  root 'boards#index'
+  resources :users, only: %i[index new create]
   resources :boards
   resources :comments, only: %i[create edit update destroy]
 end
